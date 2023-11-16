be41421653646bcb768485ec8f4b277098fb3a0e
Florian GERON <geron-f@outlook.fr>
2023-11-16 13:26:39 +0100

    terre 3

--- terre01.js
+++ terre01.js
@@ -1,3 +1,8 @@
-const exo = "terre01";
+// Importer le module path
+let path = require("path");
 
-console.log(exo);
+// Obtenir le nom du fichier en cours d'exécution avec la fonction basename
+let filename = path.basename(__filename);
+
+// Afficher le nom du fichier à la console
+console.log(filename);
---
+++ terre02.js
@@ -0,0 +1,4 @@
+// Boucler à travers tous les arguments
+for (let i = 2; i < process.argv.length; i++) {
+  console.log(process.argv[i]);
+}
