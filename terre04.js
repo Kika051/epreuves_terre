148f1b529c597013c5aa30861cb6f04003ef5849
Florian GERON <geron-f@outlook.fr>
2023-11-16 18:56:40 +0100

    terre04

--- terre04.js
+++ terre04.js
@@ -1,9 +1,12 @@
+// Vérifier s'il y a un argument
 if (process.argv.length !== 3) {
   console.log("Veuillez fournir un entier en argument.");
 } else {
+  //transformé l'argument en nombre
   const nombre = parseInt(process.argv[2]);
-
+  // Vérifier si l'argument est un nombre entier
   if (!isNaN(nombre) && Number.isInteger(nombre)) {
+    // Vérifier si l'entier est pair ou impair
     if (nombre % 2 === 0) {
       console.log(`${nombre} est un entier pair.`);
     } else {
