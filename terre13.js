dbdc5b8678f8027558427e1ab48ce009f85f0bf7
Florian GERON <geron-f@outlook.fr>
2023-11-20 17:50:55 +0100

    terre13

---
+++ terre13.js
@@ -0,0 +1,23 @@
+//Créer a la condition pour désigné la suisse
+function trouvélasuisse(a, b, c) {
+  if ((a < b && b < c) || (c < b && b < a)) {
+    return b;
+  } else if ((b < a && a < c) || (c < a && a < b)) {
+    return a;
+  } else {
+    return c;
+  }
+}
+
+// entré les arguments dans le terminal
+
+const nombre1 = parseInt(process.argv[2], 10);
+const nombre2 = parseInt(process.argv[3], 10);
+const nombre3 = parseInt(process.argv[4], 10);
+
+if (!isNaN(nombre1) && !isNaN(nombre2) && !isNaN(nombre3)) {
+  const resultat = trouvélasuisse(nombre1, nombre2, nombre3);
+  console.log("La valeur du milieu est :", resultat);
+} else {
+  console.log("Veuillez fournir trois entiers en arguments.");
+}
